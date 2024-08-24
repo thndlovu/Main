10b9a8fd43b12f3b39efda7d916729f3d634d043
thndlovu <144265291+thndlovu@users.noreply.github.com>
2024-08-23 20:01:42 -0700

    Updated App.java for specification on frame.

--- Snake Game/src/App.java
+++ Snake Game/src/App.java
@@ -6,19 +6,16 @@ public class App {
         int boardheight = boardWidth;
 
         JFrame frame = new JFrame("Snake");
-        SnakeGame snakeGame = new SnakeGame(boardWidth, boardheight);
-        frame.add(snakeGame);
-
         frame.setVisible(true);
         frame.setSize(boardWidth, boardheight);  //600 x 600
-        frame.setLocation(null); //center of screen
+        frame.setLocationRelativeTo(null); //center of screen
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //click X on window to close
+
+        SnakeGame snakeGame = new SnakeGame(boardWidth, boardheight);
+        frame.add(snakeGame);
         frame.pack();
         snakeGame.requestFocus(); //snake game is listening to key presses
-
-
-        
-        
+ 
     }
 }
